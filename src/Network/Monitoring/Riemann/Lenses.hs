c359f924f0d5264bc3d84dde07f165380c056008
Joseph Abrahamson <me@jspha.com>
2013-02-01 12:05:43 -0500

    * src/Network/Monitoring/Riemann/Lenses.hs: Switched to TH derived lenses

--- src/Network/Monitoring/Riemann/Lenses.hs
+++ src/Network/Monitoring/Riemann/Lenses.hs
@@ -1,24 +1,50 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
+{-%
+
+Interacting with big records like 'Event' and 'State' is challenging
+and requires tracking two different function namespaces for the
+getters and setters. This module rewraps all of those as lenses
+distributed across a few type classes to make interacting with
+'metric' and the overlap between 'Event' and 'State' easier.
+
+This provides a much cleaner interface to the underlying Riemann types
+costing only some clarity in types. This allows you to build and edit
+Events using chains of lens updates like
+
+>>> (attributes' .~ [("foo", Just "bar")) . (once .~ Nothing)
+
+
+I also provide 'Monoid' instances for times when combining 'Event's or
+'State's or 'Query's makes the most sense.
+
+-}
+
 module Network.Monitoring.Riemann.Lenses (
-  Stated (..),
+  Stated (..), Int64,
   Metricable (..),
   Event,
-  attributes,
-  State,
-  service, once,
-  Msg,
-  ok, merror, states, query, events,
-  Query,
-  qstring
+  attributes, attributes', attributeMap,
+  -- State,
+  -- once,
+  -- Msg,
+  -- ok, merror, states, query, events,
+  -- Query,
+  -- qstring
   ) where
+
+import GHC.Int (Int64)
 
 import Control.Lens
 import Control.Applicative
+import Control.Arrow
+import Control.Error
 import Data.Monoid
+import Data.Maybe
 import Data.Foldable
-import Data.Sequence as Sequence
+import qualified Data.Sequence as Sequence
 import           Data.Map.Lazy (Map)
 import qualified Data.Map.Lazy as M
 import           Data.Text.Lazy (Text)
@@ -35,64 +61,89 @@ import qualified Network.Monitoring.Riemann.Proto.Attribute as At
 import qualified Network.Monitoring.Riemann.Proto.Msg as Ms
 import qualified Network.Monitoring.Riemann.Proto.Query as Qu
 
-toutf8 :: Text -> Utf8
-toutf8 = Utf8 . TE.encodeUtf8 
+utfI :: Simple Iso Utf8 Text
+utfI = iso (\(Utf8 lbs) -> TE.decodeUtf8 lbs) (Utf8 . TE.encodeUtf8)
 
-fromutf8 :: Utf8 -> Text
-fromutf8 (Utf8 lbs) = TE.decodeUtf8 lbs
-
+-- | 'Stated' types are 'Event' and 'State' which both have
+-- information representing the state of a 'service' on a 'host' at a
+-- given 'time'. These shared types give rise to restrictedly
+-- polymorphic lenses.
 class Stated a where
   time        :: Simple Lens a (Maybe Int64)
   state       :: Simple Lens a (Maybe Text)
+  service     :: Simple Lens a (Maybe Text)
   host        :: Simple Lens a (Maybe Text)
   description :: Simple Lens a (Maybe Text)
   tags        :: Simple Lens a (Seq Text)
   ttl         :: Simple Lens a (Maybe Float)
 
+-- | 'Metricable' types are those which can be 'metric's in an
+-- 'Event'. This class provides dispatch from Haskell's polymorphic
+-- types to Protobuf's non-polymorphic types.
 class Metricable a where
   metric :: Simple Lens Event (Maybe a)
 
+{-%
+
+We automatically generate the core lenses, some of which will be
+lifted into an instance of the `Stated` instance.
+
+-}
+
+$(makeLensesFor [("time", "evTime"),
+                 ("state", "evState"),
+                 ("service", "evService"),
+                 ("host", "evHost"),
+                 ("description", "evDescription"),
+                 ("tags", "evTags"),
+                 ("ttl", "evTtl"),
+                 ("attributes", "evAttributes"),
+                 ("metric_sint64", "evMetric_sint64"),
+                 ("metric_d", "evMetric_d"),
+                 ("metric_f", "evMetric_f")]
+  ''Event)
+
 instance Stated Event where
-  time f ev = fmap (\a -> ev {Ev.time = a}) $ f (Ev.time ev)
+  time = evTime
+  state = evState . mapping utfI
+  service = evService . mapping utfI
+  host = evHost . mapping utfI
+  description = evDescription . mapping utfI
+  tags = evTags . mapping utfI
+  ttl = evTtl
 
-  state f ev =
-    fmap (\a -> ev {Ev.state = fmap toutf8 a})
-    $ f (fmap fromutf8 $ Ev.state ev)
+-- | Lensing 'At.Attributes'
+attIso :: Simple Iso At.Attribute (Text, Maybe Text)
+attIso = iso unAtt reAtt
+  where
+    unAtt :: At.Attribute -> (Text, Maybe Text)
+    unAtt (At.Attribute { At.key = k, At.value = v }) =
+      (view utfI k, fmap (view utfI) v)
+    reAtt :: (Text, Maybe Text) -> At.Attribute
+    reAtt (k, v) = At.Attribute { At.key = review utfI k,
+                                  At.value = fmap (review utfI) v }
 
-  host f ev =
-    fmap (\a -> ev {Ev.host = fmap toutf8 a})
-    $ f (fmap fromutf8 $ Ev.host ev)
 
-  description f ev =
-    fmap (\a -> ev {Ev.description = fmap toutf8 a})
-    $ f (fmap fromutf8 $ Ev.description ev)
+attributes' :: Simple Lens Event (Seq (Text, Maybe Text))
+attributes' = evAttributes . mapping attIso
+  where z = 3
 
-  tags f ev =
-    fmap (\a -> ev {Ev.tags = fmap toutf8 a})
-    $ f (fmap fromutf8 $ Ev.tags ev)
+attributes :: Simple Lens Event [(Text, Text)]
+attributes = attributes' . iso (catMaybes . map liftMay . toList)
+                               (Sequence.fromList . map (second Just))
+  where liftMay (a, b) = (,) <$> pure a <*> b
 
-  ttl f ev = fmap (\a -> ev {Ev.ttl = a}) $ f (Ev.ttl ev)
-
-attributes :: Simple Lens Event (Map Text (Maybe Text))
-attributes f ev = fmap (\a -> ev {Ev.attributes = enter a}) $ f (exit $ Ev.attributes ev)
-  where exit :: Seq At.Attribute -> Map Text (Maybe Text)
-        exit = foldMap (\attr -> M.singleton
-                                 (fromutf8 $ At.key attr)
-                                 (fmap fromutf8 $ At.value attr))
-        enter :: Map Text (Maybe Text) -> Seq At.Attribute
-        enter = M.foldrWithKey
-                (\k a b -> b |> At.Attribute { At.key = toutf8 k,
-                                               At.value = fmap toutf8 a })
-                mempty
+attributeMap :: Simple Lens Event (Map Text Text)
+attributeMap = attributes . iso M.fromList M.toList
 
 instance Metricable Int64 where
-  metric f ev = fmap (\a -> ev {Ev.metric_sint64 = a}) $ f (Ev.metric_sint64 ev)
+  metric = evMetric_sint64
 
 instance Metricable Double where
-  metric f ev = fmap (\a -> ev {Ev.metric_d = a}) $ f (Ev.metric_d ev)
+  metric = evMetric_d
 
 instance Metricable Float where
-  metric f ev = fmap (\a -> ev {Ev.metric_f = a}) $ f (Ev.metric_f ev)
+  metric = evMetric_f
 
 instance Monoid Event where
   mempty = defaultValue
@@ -132,37 +183,33 @@ instance Monoid Event where
                   Ev.metric_f = mf1 +++ mf2 })
     where a +++ b = getLast $ Last a <> Last b
           comb x a b = liftA2
-                       (\st1 st2 -> toutf8 $ fromutf8 st1 <> x <> fromutf8 st2)
+                       (\st1 st2 -> review utfI $ view utfI st1 <> x <> view utfI st2)
                        a b
-          
+
+
+$(makeLensesFor [("time", "stTime"),
+                 ("state", "stState"),
+                 ("service", "stService"),
+                 ("host", "stHost"),
+                 ("description", "stDescription"),
+                 ("once", "stOnce"),
+                 ("tags", "stTags"),
+                 ("ttl", "stTtl")]
+  ''State)
 
 instance Stated State where
-  time f ev = fmap (\a -> ev {St.time = a}) $ f (St.time ev)
+  time = stTime
+  state = stState . mapping utfI
+  service = stService . mapping utfI
+  host = stHost . mapping utfI
+  description = stDescription . mapping utfI
+  tags = stTags . mapping utfI
+  ttl = stTtl
 
-  state f ev =
-    fmap (\a -> ev {St.state = fmap toutf8 a})
-    $ f (fmap fromutf8 $ St.state ev)
-
-  host f ev =
-    fmap (\a -> ev {St.host = fmap toutf8 a})
-    $ f (fmap fromutf8 $ St.host ev)
-
-  description f ev =
-    fmap (\a -> ev {St.description = fmap toutf8 a})
-    $ f (fmap fromutf8 $ St.description ev)
-
-  tags f ev =
-    fmap (\a -> ev {St.tags = fmap toutf8 a})
-    $ f (fmap fromutf8 $ St.tags ev)
-
-  ttl f ev = fmap (\a -> ev {St.ttl = a}) $ f (St.ttl ev)
-
-service :: Simple Lens State (Maybe Text)
-service f ev = fmap (\a -> ev {St.service = fmap toutf8 a})
-               $ f (fmap fromutf8 $ St.service ev)
-
+-- This could just be done during the lens derivation, but this is
+-- more explicit.
 once :: Simple Lens State (Maybe Bool)
-once f ev = fmap (\a -> ev {St.once = a}) $ f (St.once ev)
+once = stOnce
 
 instance Monoid State where
   mempty = defaultValue
@@ -193,32 +240,31 @@ instance Monoid State where
                  St.ttl = liftA2 max tl1 tl2 })
     where a +++ b = getLast $ Last a <> Last b
           comb x a b = liftA2
-                       (\st1 st2 -> toutf8 $ fromutf8 st1 <> x <> fromutf8 st2)
+                       (\st1 st2 -> review utfI $ view utfI st1 <> x <> view utfI st2)
                        a b
           
 
+$(makeLensesFor [("ok", "msgOk"),
+                 ("error", "msgError"),
+                 ("states", "msgStates"),
+                 ("query", "msgQuery"),
+                 ("events", "msgEvents")]
+  ''Msg)
 
 ok :: Simple Lens Msg (Maybe Bool)
-ok f ev =
-  fmap (\a -> ev {Ms.ok = a}) $ f (Ms.ok ev)
+ok = msgOk
 
 merror :: Simple Lens Msg (Maybe Text)
-merror f ev =
-  fmap (\a -> ev {Ms.error = fmap toutf8 a})
-  $ f (fmap fromutf8 $ Ms.error ev)
+merror = msgError . mapping utfI
 
 states :: Simple Lens Msg (Seq State)
-states f ev =
-  fmap (\a -> ev {Ms.states = a}) $ f (Ms.states ev)
+states = msgStates
 
 query :: Simple Lens Msg (Maybe Query)
-query f ev =
-  fmap (\a -> ev {Ms.query = a})
-  $ f (Ms.query ev)
+query = msgQuery
 
 events :: Simple Lens Msg (Seq Event)
-events f ev =
-  fmap (\a -> ev {Ms.events = a}) $ f (Ms.events ev)
+events = msgEvents
 
 instance Monoid Msg where
   mempty = defaultValue
@@ -241,9 +287,10 @@ instance Monoid Msg where
     where a +++ b = getLast $ Last a <> Last b
 
 
+$(makeLensesFor [("string", "qString")] ''Query)
+
 qstring :: Simple Lens Query (Maybe Text)
-qstring f ev = fmap (\a -> ev {Qu.string = fmap toutf8 a})
-               $ f (fmap fromutf8 $ Qu.string ev)
+qstring = qString . mapping utfI
 
 instance Monoid Query where
   mempty = defaultValue
