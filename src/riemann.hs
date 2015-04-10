fbe580f9cea11c72d0d63bfc4cf880f27d96f98c
Arnaud Bailly <arnaud.oqube@gmail.com>
2015-04-10 13:15:12 +0200

    simple HTTP checker

--- src/riemann.hs
+++ src/riemann.hs
@@ -1,13 +1,44 @@
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import           Control.Applicative
 import           Control.Concurrent
+import           Control.Error
+import           Control.Exception          (catch)
+import           Control.Lens
 import           Control.Monad
+import           Control.Monad.IO.Class
+import           Data.Monoid                ((<>))
+import           Data.Time.Clock
+import           Network.HTTP.Client        (HttpException (..))
 import           Network.Monitoring.Riemann
+import           Network.Wreq
 import           System.Environment
 
 main :: IO ()
 main = do
-  [h,p,n] <- getArgs
-  c <- makeTCPClient h (read p)
-  forM_ [1 .. read n :: Int ] (\k -> (sendEvent c $ ev "myservice" k) >> threadDelay 1000000)
+  [rhost,rport,uriList] <- getArgs
+  c <- makeTCPClient rhost (read rport)
+  uris <- lines <$> readFile uriList
+  forever $ checkAll c uris
+
+
+checkAll :: Client -> [ String ] -> IO ()
+checkAll c uris =  forM_ uris (check c) >> threadDelay (60 * 1000000)
+
+check :: Client -> String -> IO ()
+check c uri = do
+  putStr $ "checking " <> uri <> " "
+  st <- getCurrentTime
+  ok <- (get uri >> return True) `catch` \ (_ :: HttpException) -> return False
+  en <- getCurrentTime
+  let event = ev ("http " <> uri) (realToFrac (diffUTCTime en st) :: Double) & state ?~ if ok then "ok" else "error"
+  res <- liftIO $ runEitherT $ sendEvent' c event
+  -- not very useful error handling, if socket fails to connect at startup it cannot reconnects later
+  either
+    (\e -> putStrLn ("error connecting to riemann server: " <> show e))
+    (const $ putStrLn "sent event to riemann")
+    res
 
